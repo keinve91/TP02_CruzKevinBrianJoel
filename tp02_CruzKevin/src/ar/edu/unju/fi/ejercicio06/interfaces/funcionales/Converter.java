2fd9af0085b2aefe2c167d85bebbe88eae8127d9
Joel Cruz <Joel Cruz@DESKTOP-ITJELBR>
2024-06-27 20:48:07 -0300

    Finalizacion del Ejercicio 06 - Cruz Kevin

--- tp02_CruzKevin/src/ar/edu/unju/fi/ejercicio06/interfaces/funcionales/Converter.java
+++ tp02_CruzKevin/src/ar/edu/unju/fi/ejercicio06/interfaces/funcionales/Converter.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.ejercicio06.interfaces.funcionales;
 
-public class Converter {
+@FunctionalInterface
+public interface Converter<T, R> {
+    R convert(T t);
 
+    default void mostrarObjeto(R r) {
+        System.out.println("Objeto - " + r.toString());
+    }
+
+    static <T> boolean isNotNull(T t) {
+        return t != null;
+    }
 }
