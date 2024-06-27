cec7d56163632bb0c473fc72e575177440dd32fd
Joel Cruz <Joel Cruz@DESKTOP-ITJELBR>
2024-06-27 20:25:16 -0300

    Finalizacion del Ejercicio 03 - Cruz Kevin

--- tp02_CruzKevin/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ tp02_CruzKevin/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.fi.ejercicio03.main;
-
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
 public class Main {
+	public static void main(String[] args) {
+	       Provincia[] provincias = Provincia.values();
+	       for (int i = 0; i < provincias.length; i++) {
 
+				System.out.println("Detalles de la Provincia: " + provincias[i]);
+				System.out.println("Poblacion: " + provincias[i].getPoblacion());
+				System.out.println("Superficie: " + provincias[i].getSuperficie());
+				System.out.println("Densidad Poblacional: " + provincias[i].calcularDensidadPoblacional() + "\n");
+	}
+	}
 }
